144e9cc532c9f7cf9763ba1b9650c79aaddcbf5d
Amarthya Ravi <amarthya10@gmail.com>
2020-10-05 09:09:06 +0530

    Comments added, 5th Sep 20

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+// ? Importing required libraries
 var express = require("express");
 var PORT    = process.env.PORT || 3000;
 
+// ? Importing routes
 var indexRoutes = require("./routes/index");
 
+// ? Adding required plug ins
 var app     = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.use(indexRoutes);
 
+// ? Run App
 app.listen(PORT, () => {
     console.log("Connected!");
 })
