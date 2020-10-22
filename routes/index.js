23da742b04a9a238f226bb3eb8162700a3c6ba70
Pandani07 <anidutt27@gmail.com>
2020-10-22 10:01:46 +0530

    Inital_changes_ani

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var router  = express.Router();
 
+
 router.get("/", (req, res) => {
     res.redirect("/index");
 })
 
 router.get("/index", (req, res) => {
     res.render("index");
+})
+
+router.get("/contacts", (req, res)=>{
+    res.render("contacts");
+})
+
+router.get("/pricing", (req, res)=>{
+    res.render("pricing");
+})
+
+router.get("/gallery", (req, res)=>{
+    res.render("gallery");
 })
 
 module.exports = router;
